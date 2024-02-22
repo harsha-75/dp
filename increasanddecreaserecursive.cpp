e37406feecf282d65dd1129de8f699d272777c03
harsha vardhan <harshavardhanmanne410@gmail.com>
2024-02-23 00:25:21 +0530

    first

---
+++ increasanddecreaserecursive.cpp
@@ -0,0 +1,85 @@
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+using namespace std;
+void increaseprint(int n,int i)
+{
+	   cout<<i<<" ";
+       if(i==n)
+       {
+       	  return;
+       }
+       increaseprint(n,i+1);
+}
+int powerofnum(int a,int n)
+{
+	  if(n==0)
+	  {
+	  	  return 1;
+	  }
+	 return  a* powerofnum(a,n-1);
+}
+int main()
+{
+	#ifndef ONLINNE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+   #endif
+
+	int n,a;
+	cin>>n;
+	cin>>a;
+	cout<<powerofnum(a,n);
+	    
+}
